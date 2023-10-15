306223fddce0e40600626cdaea08eb77e0dd6c2e
Ping Song <ping.song@monash.edu>
2023-10-15 16:40:42 +1100

    update

---
+++ src/components/home/Canvas.jsx
@@ -0,0 +1,71 @@
+import React, { useRef, useEffect, useState, useCallback } from 'react'
+
+const Canvas = (props) => {
+  const {
+    // width = '100%',
+    // height = '100%',
+    draw,
+    establishContext,
+    establishCanvasWidth,
+    ...rest
+  } = props
+  const canvasRef = useRef()
+  const [context, setContext] = useState(null)
+
+  useEffect(() => {
+    if (canvasRef.current) {
+      const ctx = canvasRef.current.getContext('2d')
+
+      if (ctx) {
+        ctx.canvas.width = window.innerWidth
+        ctx.canvas.height = window.innerHeight
+      }
+      setContext(ctx)
+      if (establishContext) {
+        establishContext(ctx)
+      }
+      if (establishCanvasWidth) {
+        establishCanvasWidth(ctx.canvas.width)
+      }
+    }
+  }, [canvasRef, establishCanvasWidth, establishContext])
+
+  const resizeCanvas = (context) => {
+    const canvas = context.canvas
+    const { width, height } = canvas.getBoundingClientRect()
+
+    if (canvas.width !== width || canvas.height !== height) {
+      const { devicePixelRatio: ratio = 1 } = window
+      canvas.width = width * ratio
+      canvas.height = height * ratio
+      context.scale(ratio, ratio)
+      return true
+    }
+    return false
+  }
+
+  useEffect(() => {
+    // let frameCount = 0
+    let animationFrameId
+
+    // Check if null context has been replaced on component mount
+    if (context) {
+      //Our draw came here
+      const render = () => {
+        // frameCount++
+        draw()
+        resizeCanvas(context)
+
+        animationFrameId = window.requestAnimationFrame(render)
+      }
+      render()
+    }
+    return () => {
+      window.cancelAnimationFrame(animationFrameId)
+    }
+  }, [draw, context])
+
+  return <canvas ref={canvasRef} {...rest} />
+}
+
+export default Canvas
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef, useState, useCallback } from 'react'
 import { Link } from 'react-router-dom'
 import c from './home.module.scss'
 import landingImg from '../assets/landing-browser.png'
@@ -8,6 +8,7 @@ import library from '../assets/library-browser.png'
 
 import transition from '../layout/transition'
 import Footer from '../utils/Footer'
+import Canvas from './Canvas'
 
 const Home = () => {
   useEffect(() => {
@@ -18,18 +19,137 @@ const Home = () => {
     }
   }, [])
 
+  const [ctx, setCtx] = useState(null)
+  const [canvasWidth, setCanvasWidth] = useState(null)
+  const establishContext = (context) => {
+    setCtx(context)
+  }
+
+  const establishCanvasWidth = (width) => {
+    setCanvasWidth(width)
+  }
+
+  var colors = ['#f6d1c5', '#7fd3e5']
+
+  var gravity = 0.2
+  var friction = 0.4
+
+  // Utility Functions
+  function randomIntFromRange(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+  }
+
+  function randomColor(colors) {
+    return colors[Math.floor(Math.random() * colors.length)]
+  }
+
+  function Ball(x, y, dx, dy, radius, color) {
+    this.x = x
+    this.y = y
+    this.dx = dx
+    this.dy = dy
+    this.radius = radius
+    this.color = color
+
+    this.update = function () {
+      if (this.y + this.radius + this.dy > ctx.canvas.height) {
+        this.dy = -this.dy
+        this.dy = this.dy * friction
+        this.dx = this.dx * friction
+      } else {
+        this.dy += gravity
+      }
+
+      if (
+        this.x + this.radius >= ctx.canvas.width ||
+        this.x - this.radius <= 0
+      ) {
+        this.dx = -this.dx * friction
+      }
+
+      this.x += this.dx
+      this.y += this.dy
+      this.draw()
+    }
+
+    this.draw = function () {
+      ctx.beginPath()
+      ctx.arc(this.x - 0.5, this.y - 10.5, this.radius, 0, Math.PI * 2, false)
+      ctx.fillStyle = this.color
+      ctx.fill()
+      ctx.stroke()
+      ctx.lineWidth = 1
+      ctx.closePath()
+    }
+  }
+
+  const ballArray = []
+  const drawBalls = (pink = false, blue = false) => {
+    for (let i = 0; i < 20; i++) {
+      var radius = randomIntFromRange(8, 40)
+      var x = randomIntFromRange(radius, ctx.canvas.width - radius)
+      var y = 0
+      var dx = randomIntFromRange(-3, 3)
+      var dy = randomIntFromRange(-2, 2)
+      if (pink) {
+        ballArray.push(new Ball(x, y, dx, dy, radius, colors[0]))
+      } else if (blue) {
+        ballArray.push(new Ball(x, y, dx, dy, radius, colors[1]))
+      } else {
+        ballArray.push(new Ball(x, y, dx, dy, radius, randomColor(colors)))
+      }
+    }
+  }
+  // useEffect(() => {
+  //   if (ctx) {
+  //     //  drawBalls()
+  //   }
+  // }, [ctx])
+
+  const draw = () => {
+    if (ctx) {
+      ctx.fillStyle = 'rgba(255,255,255)'
+      ctx.fillRect(0.5, 0.5, ctx.canvas.width - 0.5, ctx.canvas.height - 0.5)
+
+      for (let i = 0; i < ballArray.length; i++) {
+        ballArray[i].update()
+      }
+    }
+  }
+  const drawPink = () => {
+    drawBalls(true, false)
+  }
+
+  const drawBlue = () => {
+    drawBalls(false, true)
+  }
+
   return (
-    <div>
+    <div className={c.landingContainer}>
+      <div className={c.canvasContainer}>
+        {/* <canvas ref={canvasRef}></canvas> */}
+        <Canvas
+          draw={draw}
+          establishContext={establishContext}
+          establishCanvasWidth={establishCanvasWidth}
+        />
+      </div>
       <div className={c.landing}>
         <div className={c.intro}>
           <div>Hello, I'm Ping Song,</div>
           <div>
             a passionate{' '}
-            <span className={[c.tag, c.one].join(' ')}>
+            <span
+              className={[c.tag, c.one, c.design].join(' ')}
+              onClick={drawPink}
+            >
               UX/UI&nbsp;Designer
             </span>
             and{' '}
-            <span className={[c.tag, c.three].join(' ')}>
+            <span
+              className={[c.tag, c.three, c.design].join(' ')}
+              onClick={drawBlue}
+            >
               Front&nbsp;end&nbsp;Developer
             </span>
           </div>
